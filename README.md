6b33ab3f43cec566a370e426d6c1eea25897ee53
yunongcao <Dexter@172-16-26-217.DYNAPOOL.NYU.EDU>
2016-07-15 14:35:31 -0400

    first update

---
+++ README.md
@@ -0,0 +1,27 @@
+# WiFind Project Website Demo
+This is a bootstrap page modified for project website.  
+To do list:  
+
+### Update:
+
+* Changed section order to Map-App-About-Contact
+* Fixed some terms (e.g. contact us-contact; SSID-netowrk name)
+* Fixed the link of download page
+* Increased the height of map
+*  *  *
+
+* Removed dependencies for Angular.js, Jquery, Heatmap.js, Wifimapping Api
+* Corrected some grammar mistakes
+* Added static colorbar legend under the map
+*  *  *
+
+* Now use actual map as background of intro 
+* Fixed spacing issue on mobile devices
+* Preload heatmap for NYU
+
+
+### Future Work: 
+
+* Autocomplete SSID field
+* Change color for grayscale
+* Add legend for grayscale
---
+++ js/index1.js
@@ -0,0 +1,134 @@
+function median(values) {
+    values.sort((a, b) => a - b);
+    return (values[(values.length - 1) >> 1] + values[values.length >> 1]) / 2
+}
+
+function max(values) {
+    return values.sort((a, b) => a - b)[values.length - 1];
+}
+
+function mean(values) {
+    return (values.reduce((a, b) => a + b, 0) / values.length);
+}
+
+function range(values) {
+    values.sort((a, b) => a - b);
+    return [values[10], values[values.length-10]];
+}
+
+	
+// Create additional Control placeholders for Leaflet
+function addControlPlaceholders(map) {
+    var corners = map._controlCorners,
+        l = 'leaflet-',
+        container = map._controlContainer;
+
+    function createCorner(vSide, hSide) {
+        var className = l + vSide + ' ' + l + hSide;
+
+        corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+    }
+
+    createCorner('verticalcenter', 'left');
+    createCorner('verticalcenter', 'right');
+}
+
+
+var AGGREGATION = {
+    median: median,
+    max: max,
+    mean: mean
+};
+
+
+angular
+.module('Heatmap', [])
+.controller('Ctrl', function($scope) {
+    $scope.params = {
+        aggregation: 'median',
+        ssid: 'nyu'
+    };
+
+    var resultCache = {};
+
+    var baseLayer = L.tileLayer(
+      'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; CartoDB',
+        maxZoom: 17
+      }
+    );
+    
+//    var heatmapLayer = L.tileLayer(
+//          'http://capstone.cloudapp.net/wifipulling/tile/{z}/{x}/{y}/?ssid={ssid}&agg_function={agg_function}', {
+//            ssid: 'nyu',
+//            agg_function: 'median',
+//            maxZoom: 18,
+//            opacity: .5
+//          }
+//        );
+    
+//    var greyLayer = L.tileLayer(
+//          'http://capstone.cloudapp.net/wifipulling/greyTile/{z}/{x}/{y}', {
+//            maxZoom: 18,
+//            opacity: .5
+//          }
+//        );
+    var heatmapLayer;
+    var greyLayer;  
+
+    var map = new L.Map('map', {
+      center: new L.LatLng(40.7295134, -73.9964609),
+      zoom: 14,
+      maxZoom: 17,
+      minZoom: 12,
+      scrollWheelZoom: false,  
+      layers: [baseLayer]  
+    });
+    
+    addControlPlaceholders(map);
+    map.zoomControl.setPosition('verticalcenterleft');
+//    map.addLayer(greyLayer);
+//    map.addLayer(heatmapLayer);
+
+
+    map.on('zoomstart', function() {
+        window.stop();
+    });
+
+    $scope.execute = function() {
+        if (heatmapLayer) {
+            map.removeLayer(heatmapLayer);
+        } else {
+            map.on('zoomend', function() {
+                $scope.execute();
+            });
+        }
+        
+        greyLayer = L.tileLayer(
+          'http://capstone.cloudapp.net/wifipulling/greyTile/{z}/{x}/{y}', {
+            maxZoom: 18,
+            opacity: .5
+          }
+        );
+        
+        map.addLayer(greyLayer);
+
+        heatmapLayer = L.tileLayer(
+          'http://capstone.cloudapp.net/wifipulling/tile/{z}/{x}/{y}/?ssid={ssid}&agg_function={agg_function}', {
+            ssid: $scope.params.ssid,
+            agg_function: $scope.params.aggregation,
+            maxZoom: 18,
+            opacity: .5
+          }
+        );
+
+        map.addLayer(heatmapLayer);
+
+        map.on('zoomend', function() {
+            $scope.execute();
+        });
+
+    };
+});
+    
+$scope.execute
